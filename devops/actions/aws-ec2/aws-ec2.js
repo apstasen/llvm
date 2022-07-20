940eb21d0d5b7d2b70400564175c8a34687fc4d3
apstasen <alexander.p.stasenko@intel.com>
2022-07-20 14:55:19 -0700

    Update aws-ec2.js

--- devops/actions/aws-ec2/aws-ec2.js
+++ devops/actions/aws-ec2/aws-ec2.js
@@ -3,11 +3,8 @@ const github = require('@actions/github');
 const AWS    = require('aws-sdk');
 
 async function main() {
-  AWS.config = new AWS.Config();
-  AWS.config.accessKeyId     = core.getInput("AWS_ACCESS_KEY_ID");
-  AWS.config.secretAccessKey = core.getInput("AWS_SECRET_ACCESS_KEY");
-
-  const ec2 = new AWS.EC2({region: core.getInput("aws-region")});
+  AWS.config.update({ accessKeyId: core.getInput("AWS_ACCESS_KEY_ID"), secretAccessKey: core.getInput("AWS_SECRET_ACCESS_KEY") });
+    const ec2 = new AWS.EC2({region: core.getInput("aws-region")});
   
   const reg_token = core.getInput("RUNNER_REG_TOKEN");
   const label     = "aws_" + Math.random().toString(36).substr(2, 7);
