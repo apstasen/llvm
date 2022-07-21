d12a37c6e54b44c6ea6e37e332241a4fd79364f0
apstasen <alexander.p.stasenko@intel.com>
2022-07-20 18:13:06 -0700

    Update aws-ec2.js

--- devops/actions/aws-ec2/aws-ec2.js
+++ devops/actions/aws-ec2/aws-ec2.js
@@ -8,9 +8,6 @@ async function main() {
     secretAccessKey: core.getInput("AWS_SECRET_KEY"),
     region:          core.getInput("aws-region")
   });
-  const test = core.getInput("aws-access-key").length;
-  const test2 = process.env["aws-access-key"].length
-  core.info(`XXX ${test} ${test2}`);
   const ec2 = new AWS.EC2();
   
   const reg_token = core.getInput("RUNNER_REG_TOKEN");
