4ef1c44a03807b70e8124e4c382cd6454d761891
apstasen <alexander.p.stasenko@intel.com>
2022-08-26 14:33:31 -0700

    Update DummyYAML.cpp

--- llvm/benchmarks/DummyYAML.cpp
+++ llvm/benchmarks/DummyYAML.cpp
@@ -3,10 +3,10 @@
 
 static void BM_YAMLDummyIsNumeric(benchmark::State& state) {
   std::string x = "hello";
-  for (auto _ : state) {
-    std::string copy(x);
-    llvm::yaml::isNumeric(copy);
-  }
+for (auto _ : state) {
+std::string copy(x);
+llvm::yaml::isNumeric(copy);
+}
 }
 BENCHMARK(BM_YAMLDummyIsNumeric);
 
